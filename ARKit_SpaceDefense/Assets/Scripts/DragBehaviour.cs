5614d6f97657faf915453fe83837b6e9b5f68c2b
Marek <marek.marchlewicz@gmail.com>
2017-08-17 18:38:12 +0800

    Added simple draggin from UI

---
+++ ARKit_SpaceDefense/Assets/Scripts/DragBehaviour.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragBehaviour : MonoBehaviour
+{
+    public event System.Action<DragBehaviour> OnDragStart, OnDragUpdate, OnDragStop;
+
+    [SerializeField]
+    private LayerMask raycastMask = 1;
+
+    [SerializeField]
+    private float maxRayDistance = 300f;
+
+    [SerializeField]
+    private float smoothing = 5f;
+
+    private const float defaultCameraDepth = 5f;
+    
+    private Transform m_Transform;
+
+    private int touchId = -1;
+    private Camera dragCamera;
+
+    private RaycastHit[] rayHitBuffer;
+
+    public bool IsDragged { get { return touchId >=0; } }
+
+    private void Awake()
+    {
+        m_Transform = GetComponent<Transform>();
+        rayHitBuffer = new RaycastHit[10];
+    }
+
+    public void StartDragging(int newTouchId, Camera newDragCamera)
+    {
+        #if UNITY_STANDALONE
+        touchId = 1;
+        #else
+        touchId = newTouchId;
+        #endif
+        dragCamera = newDragCamera;
+
+        UpdateDragging(true);
+
+        if (OnDragStart != null)
+        {
+            OnDragStart(this);
+        }
+    }
+
+    public void StopDragging()
+    {
+        if (OnDragStop != null)
+        {
+            OnDragStop(this);
+        }
+
+        touchId = -1;
+    }
+
+    private void UpdateDragging(bool immediately = false)
+    {
+        Vector2? touch = GetTouchPosition();
+
+        if (touch.HasValue)
+        {
+            int hits = Physics.RaycastNonAlloc(dragCamera.ScreenPointToRay(touch.Value), rayHitBuffer, maxRayDistance, raycastMask);
+
+            float lerp = immediately ? 1 : Time.deltaTime * smoothing;
+
+            Vector3 newPosition = Vector3.zero;
+
+            if (hits > 0)
+                newPosition = rayHitBuffer[0].point;
+            else
+                newPosition = dragCamera.ScreenToWorldPoint(new Vector3(touch.Value.x, touch.Value.y, defaultCameraDepth));
+            
+            m_Transform.position = Vector3.Lerp(m_Transform.position, newPosition, lerp);
+
+            if(OnDragUpdate != null)
+            {
+                OnDragUpdate(this);
+            }
+        }
+        else
+        {
+            StopDragging();
+        }
+    }
+
+    private void Update()
+    {
+        if (IsDragged)
+        {
+            UpdateDragging();
+        }
+    }
+
+    private Vector2? GetTouchPosition()
+    {
+        #if UNITY_STANDALONE
+        if (Input.GetMouseButton(0))
+        {
+            return Input.mousePosition;
+        }
+        #else
+        for (int t = 0; t < Input.touchCount; t++)
+        {
+            if (Input.touches[t].fingerId == touchId.Value)
+            {
+                return Input.touches[t].position;
+            }
+        }
+        #endif
+
+        return null;
+    }
+}
---
+++ ARKit_SpaceDefense/Assets/Scripts/Selectable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class Selectable : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    private GameObject draggableItem;
+
+    [SerializeField]
+    private Camera dragCamera;
+
+    private void OnValidate()
+    {
+        if(dragCamera == null)
+        {
+            dragCamera = Camera.main;
+        }
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameObject newDraggableItem = Instantiate<GameObject>(draggableItem);
+
+        DragBehaviour dragBehaviour = newDraggableItem.GetComponent<DragBehaviour>();
+        dragBehaviour.StartDragging(eventData.pointerId, dragCamera);
+
+        Debug.Log("ONPointerDown");
+    }
+}
